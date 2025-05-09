bcbb2c203b2b8ff9d33edd9f84cb2f36313626b0
DARK_FANTASY_KALANDKONYVEK <lughlleu@gmail.com>
2025-05-09 14:50:56 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// ► 1. Cache név verzió: ha módosítasz valamit, növeld pl. '…-v4'-re
-const CACHE_NAME = 'kalandkonyv-cache-v4';
+// ► 1. Cache név verzió: ha módosítasz valamit, növeld pl. '…-v5'-re
+const CACHE_NAME = 'kalandkonyv-cache-v5';
 // ► 2. Statikus fájlok (install fázisban ezeket töltjük be először)
 const STATIC_ASSETS = [
   '/',                  // start_url
